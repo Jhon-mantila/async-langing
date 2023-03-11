42fc69a8e2a350ce79db0a90043be6ad6f632ed2
Jhon-mantila <kaiser_4tu@hotmail.com>
2023-03-10 21:28:15 -0500

    terminar landing page

--- src/assets/main.js
+++ src/assets/main.js
@@ -27,6 +27,7 @@ async function fetchData(urlApi){
         const videos = await fetchData(API);
         let view = `
         ${videos.items.map(video => `
+        <a href="https://youtube.com/watch?v=${video.id.videoId}"target="_blank">
         <div class="group relative">
             <div
                 class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
@@ -37,12 +38,16 @@ async function fetchData(urlApi){
                     <span aria-hidden="true" class="absolute inset-0"></span>
                     ${video.snippet.title}
                 </h3>
+                
             </div>
+
         </div>
+        </a>
         `).slice(0,4).join('')}
 
         `;
-    } catch {
-        
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
     }
 })();
